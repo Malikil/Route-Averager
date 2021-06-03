29345bb985bdd94cec3aac6189153b619d8b690b
Eric Hagen <eboy.coltt5@gmail.com>
2021-06-02 17:40:12 -0700

    Commit old changes
    From what I see:
    * points which are too close together will be combined
    * Added a function for smoothing points on a track
    * Can now create new gpx files based on a list of points

--- src/main/kotlin/GpxHandler.kt
+++ src/main/kotlin/GpxHandler.kt
@@ -2,10 +2,18 @@ import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 import java.awt.geom.Point2D
 import java.io.File
+import java.io.FileOutputStream
 import java.io.StringReader
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
+import kotlin.collections.ArrayList
 
 class GpxHandler(
     private val filename: String
@@ -40,12 +48,109 @@ class GpxHandler(
         return@lazy trackPoints
     }
 
+    companion object {
+        fun createGpx(filename: String, track: List<Point2D>, name: String = ""): GpxHandler {
+            // These constants seem to exist as attributes in the gpx tag
+            // I have a gpx from a gps and one downloaded from strava, these
+            // are the attributes common to each
+            val XMLNS = "http://www.topografix.com/GPX/1/1"
+            val XMLNS_GPXX = "http://www.garmin.com/xmlschemas/GpxExtensions/v3"
+            val XMLNS_GPXTPX = "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
+            val XMLNS_XSI = "http://www.w3.org/2001/XMLSchema-instance"
+            val XSI_SCHEMALOCATIONS = arrayOf(
+                "http://www.topografix.com/GPX/1/1",
+                "http://www.topografix.com/GPX/1/1/gpx.xsd",
+                "http://www.garmin.com/xmlschemas/GpxExtensions/v3",
+                "http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd",
+                "http://www.garmin.com/xmlschemas/TrackPointExtension/v1",
+                "http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd"
+            )
+            val VERSION = "1.1"
+
+            val dbFactory = DocumentBuilderFactory.newInstance()
+            val dBuilder = dbFactory.newDocumentBuilder()
+            val doc = dBuilder.newDocument()
+
+            val gpxNode = doc.createElement("gpx")
+            gpxNode.setAttribute("creator", "TrackAverager")
+            gpxNode.setAttribute("xmlns:xsi", XMLNS_XSI)
+            gpxNode.setAttribute("xsi:schemaLocation", XSI_SCHEMALOCATIONS.joinToString(" "))
+            gpxNode.setAttribute("version", VERSION)
+            gpxNode.setAttribute("xmlns", XMLNS)
+            gpxNode.setAttribute("xmlns:gpxtpx", XMLNS_GPXTPX)
+            gpxNode.setAttribute("xmlns:gpxx", XMLNS_GPXX)
+
+            val meta = doc.createElement("metadata")
+            val metaTime = doc.createElement("time")
+            metaTime.appendChild(doc.createTextNode(Instant.now().toString()))
+            meta.appendChild(metaTime)
+            gpxNode.appendChild(meta)
+
+            val trk = doc.createElement("trk")
+            val trkName = doc.createElement("name")
+            trkName.appendChild(doc.createTextNode(name))
+            trk.appendChild(trkName)
+
+            val trkSeg = doc.createElement("trkseg")
+            // Add all the track points
+            track.forEach { trackPoint ->
+                val trkpt = doc.createElement("trkpt")
+                trkpt.setAttribute("lat", trackPoint.y.toString())
+                trkpt.setAttribute("lon", trackPoint.x.toString())
+                trkSeg.appendChild(trkpt)
+            }
+
+            trk.appendChild(trkSeg)
+            gpxNode.appendChild(trk)
+            doc.appendChild(gpxNode)
+            val tr = TransformerFactory.newInstance().newTransformer()
+            tr.setOutputProperty(OutputKeys.INDENT, "yes")
+            tr.setOutputProperty(OutputKeys.METHOD, "xml")
+            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8")
+
+            tr.transform(
+                DOMSource(doc),
+                StreamResult(FileOutputStream(filename))
+            )
+
+            return GpxHandler(filename)
+        }
+    }
+
     fun getTrackName(): String {
         val xpFactory = XPathFactory.newInstance()
         val xPath = xpFactory.newXPath()
         val xpath = "/gpx/trk/name"
 
         return xPath.evaluate(xpath, doc, XPathConstants.STRING) as String
+    }
+
+    /**
+     * Runs through the track and combines points that are too close together
+     * @param threshold How close together should two points be for them to be combined.
+     * The default value is equivalent to about 3 meters using lat/lon coordinates
+     */
+    fun getSimplifiedTrack(threshold: Double = 0.00003)
+            : List<Point2D> {
+        val simplified = ArrayList<Point2D>()
+
+        // Figure out the point to add
+        // I've tried about 3 different ways of doing this iteration, and I
+        // couldn't figure out any of them. Please kotlin just give me a normal
+        // for loop sometimes
+        var i = 0
+        while (i < track.size) {
+            var add = track[i++]
+            var count = 1
+            // When points are close together, subsequent points should be
+            // combined into the running average
+            while (i < track.size && track[i].distance(add) < threshold)
+                add = pointAverage(add, count++, track[i++], 1)
+            // The averaged point should be added to the simplified list
+            simplified.add(add)
+        }
+
+        return simplified
     }
 
     /**
@@ -89,5 +194,12 @@ class GpxHandler(
         }
 
         return trackPoints
+    }
+
+    private fun pointAverage(a: Point2D, aWeight: Int, b: Point2D, bWeight: Int): Point2D {
+        return a.javaClass.getConstructor(a.x.javaClass, a.y.javaClass).newInstance(
+            (a.x * aWeight + b.x * bWeight) / (aWeight + bWeight),
+            (a.y * aWeight + b.y * bWeight) / (aWeight + bWeight)
+        )
     }
 }
